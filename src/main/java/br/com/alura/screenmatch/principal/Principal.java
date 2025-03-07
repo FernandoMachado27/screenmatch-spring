2c180eaec10af4d7613cb5bc1cb8705da7068d6a
FernandoMachado27 <fernando2747@yahoo.com>
2025-03-07 10:44:15 -0300

    Trabalhando na coleção de dados

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
@@ -28,14 +29,22 @@ public class Principal {
         System.out.println("Dados da série: " + dados);
 
 		List<DadosTemporada> temporadas = new ArrayList<>();
-
 		for (int i = 1; i<= dados.totalTemporadas(); i++) {
             json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
-
 		temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        // para cada temporada t obtem a lista de episódios / para cada episódio e imprime seu título
     }
 
 }
